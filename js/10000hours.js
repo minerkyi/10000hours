ad027afe756dc3ff59c4b83b14c14e2b124680f2
김용일 <minerkyi@gmail.com>
2025-03-25 17:15:32 +0900

    1만 시간의 법칙 프로젝트 작업 중

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,12 @@
 // js file
+const dialog = document.getElementById('dialog');
+const showDialogBtn = document.getElementById('showDialogBtn');
+const closeBtn = document.getElementById('closeBtn');
+
+showDialogBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+closeBtn.addEventListener('click', () => {
+    dialog.close();
+});
